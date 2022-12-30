09a0bba5f3bcd0ff9c84897b54aad3dacb07af24
wheat <daisymak@yahoo.com>
2022-12-30 17:31:17 +0000

    updated readme. refactor the code. turn distance to scaled score

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # FacialSimilarity
+
+A [Streamlit](https://streamlit.io/)🎈 to compare facial similarity of family members. Whom does your child look like?
+
+It is powered by the deep learning library [deepface](https://github.com/serengil/deepface) which detect and compare the facial images. Simply upload the pictures of the family members (support `png`, `jpeg`, `jpg` format) and let the app analyse the images for you!
+
+## Installation
+```bash
+pip install -r requirements.txt
+```
+
+## Running locally
+This app uses Streamlit (you will install this library using the previous step). Once the neccessary python libraries are installed, then run the following in your terminal
+```bash
+streamlit run app.py
+```
+A new window will pop up in the browser to show you the app. Otherwise, go to `http://localhost:8501` in your browser to access the app.
+
+## Run in on cloud
+This app is deployed in Streamlit Cloud, you can access it [here] (https://starrywheat-deepface-app-sj1pvn.streamlit.app)
---
+++ images_utils.py
@@ -0,0 +1,188 @@
+from deepface import DeepFace
+from PIL import Image
+import numpy as np
+from typing import Dict
+from typing import Tuple
+from typing import List
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+def load_img(img: any) -> np.array:
+    """
+    This function loads the uploaded img obj and turn to np.array
+
+    Returns:
+        img_arr (np.array): img in numpy array
+    """
+    image_loaded = Image.open(img)
+    image_arr = np.array(image_loaded.convert("RGB"))
+    return image_arr
+
+
+def run_deepface(img1: any, img2: any, model_name: str, distance_metrics: str) -> Dict:
+    """
+    This function use deepface.verify to compare the img1 and img2 to get similarity
+
+    Args:
+        img1 (any): image 1
+        img2 (any): image 2
+        model_name (str): name of the deepL model for factial detection
+        distance_metrics (str): name of the distance metrics
+
+    Returns:
+        result (Dict): result of the analysis
+    # TODO: calculate similarity for specific face features, e.g. eyes
+    """
+    try:
+        result = DeepFace.verify(
+            img1_path=load_img(img1),
+            img2_path=load_img(img2),
+            model_name=model_name,
+            detector_backend="mtcnn",
+            distance_metric=distance_metrics,
+        )
+        return result
+    except ValueError:
+        return None
+
+
+def show_img(sample: bool = False) -> Tuple:
+    """
+    This function displays the upload and loaded images.
+    When the app first load, it will display sample family photos (Beckham family)
+    After that, session_state['Init'] will set to false so that it can allow user to try their own.
+
+    Args:
+    sample (bool): If True, display the family pictures in sample
+
+    Returns:
+        img_father, img_mother, img_child: The files uploaded
+    """
+    # Display the uploaded images
+    st.subheader("Upload pictures of your family to get started!")
+    st.write("hint: For best results, use headshots.")
+
+    col1, col2, col3 = st.columns(3)
+    if sample:
+        img_father = "data/father.jpeg"
+        img_child = "data/child.jpeg"
+        img_mother = "data/mother.jpeg"
+    else:
+        img_father, img_child, img_mother = None, None, None
+
+    with col1:
+        if img_father is None:
+            img_father = st.file_uploader(
+                "Upload an image for father 👨", type=["png", "jpg", "jpeg"]
+            )
+        if img_father is not None:
+            st.image(img_father, caption="Father", width=200)
+    with col2:
+        if img_child is None:
+            img_child = st.file_uploader(
+                "Upload an image for child 🧒👧", type=["png", "jpg", "jpeg"]
+            )
+        if img_child is not None:
+            st.image(img_child, caption="Child", width=200)
+    with col3:
+        if img_mother is None:
+            img_mother = st.file_uploader(
+                "Upload an image for mother 👩", type=["png", "jpg", "jpeg"]
+            )
+        if img_mother is not None:
+            st.image(img_mother, caption="Mother", width=200)
+
+    return img_father, img_mother, img_child
+
+
+def distance2score(d1: float, d2: float, distance_metrics: str) -> List:
+    if distance_metrics == "cosine":
+        s1 = 1.0 - d1
+        s2 = 1.0 - d2
+    else:
+        s1 = 1.0 / (1.0 + d1)
+        s2 = 1.0 / (1.0 + d2)
+
+    score1 = round(s1 / (s1 + s2) * 100.0, 1)
+    score2 = round(s2 / (s1 + s2) * 100.0, 1)
+
+    return [score1, score2]
+
+
+def charts(paternal_result: Dict, maternal_result: Dict, distance_metrics: str):
+    """
+    This function create a bar chart based on the analysis result
+
+    Args:
+        paternal_result (Dict): father analysis result
+        maternal_result (Dict): mother analysis result
+        distance_metrics (str): name of the distance metrics used
+    """
+    simscore = distance2score(
+        paternal_result["distance"], maternal_result["distance"], distance_metrics
+    )
+    data = {
+        "parent": ["Father", "Mother"],
+        "feature": ["face", "face"],
+        "distance": [paternal_result["distance"], maternal_result["distance"]],
+        "similarity": simscore,
+    }
+    chart_data = pd.DataFrame(data=data)
+
+    fig = px.bar(
+        chart_data,
+        x="similarity",
+        y="feature",
+        color="parent",
+        title="Similarity",
+        orientation="h",
+        height=250,
+    )
+    st.plotly_chart(fig, theme="streamlit")
+
+
+def compare_image(
+    img_father: any,
+    img_mother: any,
+    img_child: any,
+    distance_metrics: str = "cosine",
+    model_name: str = "Facenet",
+) -> Tuple:
+    """
+    This function run analysis to compare the parent-child images.
+    After successful run of the comparisons, this function also plot a bar chart
+    to display the result visually
+
+    Args:
+        img_father (any): streamlit file uploader obj for father
+        img_mother (any): streamlit file uploader obj for mother
+        img_child (any):  streamlit file uploader obj for child
+        distance_metrics (str, optional): name of the distance metrics used for comparison. Defaults to "cosine".
+        model_name (str, optional): name of the model used for facial detection. Defaults to "Facenet".
+
+    Returns:
+        paternal_result, maternal_result: analysis results
+    """
+    # Run the analysis
+    with st.spinner("Analyzing the images..."):
+        paternal_result = run_deepface(
+            img_father, img_child, model_name, distance_metrics
+        )
+        maternal_result = run_deepface(
+            img_mother, img_child, model_name, distance_metrics
+        )
+
+    # Show the results
+    if paternal_result is not None and maternal_result is not None:
+        charts(paternal_result, maternal_result, distance_metrics=distance_metrics)
+
+        if maternal_result["distance"] < paternal_result["distance"]:
+            st.success("The child looks more like mother.")
+        else:
+            st.success("The child looks more like father.")
+
+        return maternal_result["distance"], paternal_result["distance"]
+    else:
+        st.error("Something wrong with either the pictures. Upload them again")
