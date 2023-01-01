d7056b5dc4889dc1d1c6693892d496ef9a6e9a05
wheat <daisymak@yahoo.com>
2023-01-01 23:28:44 +0000

    use dlib only

--- facial_feature.py
+++ facial_feature.py
@@ -1,6 +1,22 @@
-import face_recognition
+# import face_recognition
+from collections import OrderedDict
+
+import dlib
 import numpy as np
 from PIL import Image
+
+FACIAL_LANDMARKS_68_IDXS = OrderedDict(
+    [
+        ("mouth", (48, 68)),
+        ("inner_mouth", (60, 68)),
+        ("right_eyebrow", (17, 22)),
+        ("left_eyebrow", (22, 27)),
+        ("right_eye", (36, 42)),
+        ("left_eye", (42, 48)),
+        ("nose", (27, 36)),
+        ("jaw", (0, 17)),
+    ]
+)
 
 
 def load_img(img: any) -> np.array:
@@ -58,10 +74,10 @@ def crop_img(
         print("No such feature.")
         return None
     try:
-        left = min([coords[0] for coords in landmark_coords]) * 0.9
-        top = min([coords[1] for coords in landmark_coords]) * 0.9
-        right = max([coords[0] for coords in landmark_coords]) * 1.1
-        bottom = max([coords[1] for coords in landmark_coords]) * 1.1
+        left = min([coords[0] for coords in landmark_coords]) * 0.95
+        top = min([coords[1] for coords in landmark_coords]) * 0.95
+        right = max([coords[0] for coords in landmark_coords]) * 1.05
+        bottom = max([coords[1] for coords in landmark_coords]) * 1.05
         image_cropped = image.crop((left, top, right, bottom))
         return image_cropped
     except ValueError:  # empty feature
@@ -80,5 +96,25 @@ def detect_facial_landmarks(img: any) -> list:
         list: list of the detected facial landmarks
     """
     image_array = load_img(img)
-    face_landmarks_list = face_recognition.face_landmarks(image_array)
-    return face_landmarks_list
+
+    # face_landmarks_list = face_recognition.face_landmarks(image_array)
+
+    shape_predictor = "data/shape_predictor_68_face_landmarks.dat"
+    # Initialize dlib's face detector, then facial landmark predictor
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(shape_predictor)
+
+    # detect faces
+    rects = detector(image_array, 1)
+
+    # Facial landmarks
+    facial_landmarks = []
+    for rect in rects:
+        landmarks = {}
+        # determine the facial landmarks for the face region
+        shape = predictor(image_array, rect)
+        for k, v in FACIAL_LANDMARKS_68_IDXS.items():
+            coords = [(shape.part(i).x, shape.part(i).y) for i in range(*v)]
+            landmarks[k] = coords
+        facial_landmarks.append(landmarks)
+    return facial_landmarks
